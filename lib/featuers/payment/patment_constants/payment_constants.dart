5d2a52c62b813d7358f768adf7e29ca0b07926ae
Abdullah Essam <121309175+Abdallah-Essam-Abdallah@users.noreply.github.com>
2024-03-30 16:35:37 +0200

    Update payment_constants.dart

--- lib/featuers/payment/patment_constants/payment_constants.dart
+++ lib/featuers/payment/patment_constants/payment_constants.dart
@@ -1,6 +1,6 @@
 abstract class ApiKeys {
   static const String publishableKey =
-      'pk_test_51O6I6eHKmSrjjPWfr4pJv35Zhrv1LjsmtO9CccVjrpevGxvjDxsPs9J4Rwo30UDyosUjcvTMvrIQkBH1mXosv3Xt008229JeZN';
+      'API_KEY';
   static const String secretKey =
-      'sk_test_51O6I6eHKmSrjjPWfuinKqF29ZZLelNd1SSqudRDriNoQGTeTLZtTqU5bvv1VrB4Re37CXMp4ZCfIHH9rt0u7UMHb009PtYplWD';
+      'SECRET_API_KEY';
 }
